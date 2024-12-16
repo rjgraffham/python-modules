31665aa03613dcdec4e9559e1046130b58e237a3
Robert Graffham <robert.graffham@googlemail.com>
2024-12-16 15:46:27 +0000

    Add initial skeleton for exercise 7

---
+++ TIFC1-PF-07 - User Input and While Loops/exercises/user_input_while.py
@@ -0,0 +1,31 @@
+# 7.1  Rental car
+
+
+print("---")  # Section separator for output
+
+
+# 7.2  ___
+
+
+print("---")  # Section separator for output
+
+
+# 7.3  ___
+
+
+print("---")  # Section separator for output
+
+
+# 7.4  ___
+
+
+print("---")  # Section separator for output
+
+
+# 7.5  ___
+
+
+print("---")  # Section separator for output
+
+
+# 7.6  ___
