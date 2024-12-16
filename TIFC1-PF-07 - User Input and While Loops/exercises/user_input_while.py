69f1bcec4f780c6dd0c5a45d4c9c3b6a60f6f493
Robert Graffham <robert.graffham@googlemail.com>
2024-12-16 15:51:15 +0000

    Add exercise 7 parts 1-3 (of 6)

--- TIFC1-PF-07 - User Input and While Loops/exercises/user_input_while.py
+++ TIFC1-PF-07 - User Input and While Loops/exercises/user_input_while.py
@@ -1,16 +1,28 @@
 # 7.1  Rental car
+make_of_car = input("What make of car do you want?\n")
+print(f"Okay, I'll see if I can find you a {make_of_car}.")
 
 
 print("---")  # Section separator for output
 
 
-# 7.2  ___
+# 7.2  Dinner group
+number_in_group = int(input("How many people are in your dinner group?\n"))
+if number_in_group > 8:
+    print("Sorry, you'll have to wait for a table.")
+else:
+    print("Okay, your table is ready.")
 
 
 print("---")  # Section separator for output
 
 
-# 7.3  ___
+# 7.3  Divisible by 10
+num = int(input("Enter a whole number: "))
+if num % 10 == 0:
+    print(f"{num:,} is a multiple of 10.")
+else:
+    print(f"{num:,} is not a multiple of 10.")
 
 
 print("---")  # Section separator for output
