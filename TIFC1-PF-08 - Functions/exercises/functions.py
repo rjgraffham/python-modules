c15575bc2e5d0e871f08d796ca888edf8be9f491
Robert Graffham <robert.graffham@googlemail.com>
2024-12-17 11:45:17 +0000

    Add exercise 8 parts 1-3 (of 5)

---
+++ TIFC1-PF-08 - Functions/exercises/functions.py
@@ -0,0 +1,40 @@
+# 8.1  Simple message function
+def display_message():
+    print("In this module, we are learning about functions.")
+
+display_message()
+
+
+print("---")  # Section separator for output
+
+
+# 8.2  Favourite book printer
+def favorite_book(title):
+    print(f"One of my favourite books is {title}.")
+
+favorite_book("Gideon the Ninth")
+
+
+print("---")  # Section separator for output
+
+
+# 8.3  Shirt info
+def make_shirt(size="large", message="I love Python."):
+    print(f"A {size} shirt with the message \"{message}\"")
+
+make_shirt("small", "I wrote a shirt function and all I got was this shirt")
+make_shirt(message="This shirt has the message passed first", size="medium")
+make_shirt()
+make_shirt("medium")
+
+
+print("---")  # Section separator for output
+
+
+# 8.4  ___
+
+
+print("---")  # Section separator for output
+
+
+# 8.5  ___
